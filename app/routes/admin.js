dc4ce5b9dd67fe2cf228b749bff14c35397b64f9
jacob ruleaux and nathan stewart <jacob@>
2017-08-08 13:41:34 -0700

    add delete story functionality

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('story');
+  },
+
   actions: {
     saveStory(params) {
       var newStory = this.store.createRecord('story', params);
       newStory.save();
       this.transitionTo('index');
+    },
+    delete(story) {
+        if (confirm('Are you sure you want to delete this story?')) {
+          story.destroyRecord();
+          this.transitionTo('admin');
+          }
     }
-  }  
+  }
 });
