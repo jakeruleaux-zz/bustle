e76027b315b3dc54b0c4e487f55fea1f870e5e63
jacob ruleaux and nathan stewart <jacob@>
2017-08-08 14:01:42 -0700

    add update story functionality

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -11,6 +11,15 @@ export default Ember.Route.extend({
       newStory.save();
       this.transitionTo('index');
     },
+    update(story, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          story.set(key,params[key]);
+        }
+      });
+      story.save();
+      this.transitionTo('admin');
+    },
     delete(story) {
         if (confirm('Are you sure you want to delete this story?')) {
           story.destroyRecord();
